58e8f200c4b0ed9a901a8fe1791c41f5c116cc3c
Perihan_Mohamed <perihan.mohamed92@gmail.com>
2020-12-21 15:56:15 +0200

    init

--- app/src/main/java/com/example/mymoviedatabase/ui/popular/MainActivity.kt
+++ app/src/main/java/com/example/mymoviedatabase/ui/popular/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.mymoviedatabase.ui.popular
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mymoviedatabase.R
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+
+        }
+        return true
+    }
+
 }
